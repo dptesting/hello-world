5361e43978d2c566589aea0ffd9c6d68ca5f39ae
janetowen07 <janetowen07@gmail.com>
2017-05-04 20:58:42 +0000

    added my message to debs and susie

--- index.php
+++ index.php
@@ -3,6 +3,7 @@
 //Debs trying out version control V1
 function writeMessage() {//Defining a PHP Function
     echo "Hello World and Janet and Susie. Have a nice time adding to this please.";
+    echo "Hey Debs and Susie. Are you enjoying git???";
 }
 
 writeMessage(); //Calling a PHP Function
