caeefc03cc2320b16e4872db38a0c58ddded46e0
The BOSS <debs_1_2_1@hotmail.co.uk>
2017-05-04 11:31:20 +0100

    my first commit

---
+++ index.php
@@ -0,0 +1,9 @@
+<?php
+
+//Debs trying out version control V1
+function writeMessage() {//Defining a PHP Function
+    echo "Hello World and Janet and Susie. Have a nice time adding to this please.";
+}
+
+writeMessage(); //Calling a PHP Function
+      
