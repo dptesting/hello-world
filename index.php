7892f0e43515f50dba4b8acb0cfdc9e849fd7996
debs_1_2_1@hotmail.co.uk <BossLady@deborahs-mbp.home>
2017-05-06 22:11:09 +0100

    have to get use to source tree

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
 function writeMessage() {//Defining a PHP Function
     echo "Hello World and Janet and Susie. Have a nice time adding to this please.";
     echo "Hey Debs and Susie. Are you enjoying git???";
+    echo "Susie and Jent - I dont like sourcetree";
 }
 
 writeMessage(); //Calling a PHP Function
-      
