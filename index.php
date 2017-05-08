40485fe51ea9cdf54525cf760abda2946f5abb70
debs_1_2_1@hotmail.co.uk <BossLady@deborahs-mbp.home>
2017-05-08 21:33:20 +0100

    made changes so both functions work and are called

--- index.php
+++ index.php
@@ -4,15 +4,16 @@
 function writeMessage() {//Defining a PHP Function
     echo "Hello World and Janet and Susie. Have a nice time adding to this please.";
     echo "Hey Debs and Susie. Are you enjoying git???";
-    echo "Susie and Jent - I dont like sourcetree";//head hurting
+    echo "Susie and Janet - I dont like sourcetree";//head hurting
 }
 
 writeMessage(); //Calling a PHP Function
 
 //Susie trying out version control with Debs
-function sayhello {
+function sayhello() {//added brackets
     echo "Hello Debs!";
-    echo "Hello Janet!":
+    echo "Hello Janet!";//changed to semi colon
     echo "Why tell us the coding test is harder than we expect!";
 }
 
+sayhello(); //Calling Susie function
