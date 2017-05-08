1d7db08c5f285e14ab2680ccfa48594ac186b589
debs_1_2_1@hotmail.co.uk <BossLady@deborahs-mbp.home>
2017-05-08 21:22:40 +0100

    testing

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
 function writeMessage() {//Defining a PHP Function
     echo "Hello World and Janet and Susie. Have a nice time adding to this please.";
     echo "Hey Debs and Susie. Are you enjoying git???";
-    echo "Susie and Jent - I dont like sourcetree";
+    echo "Susie and Jent - I dont like sourcetree";//head hurting
 }
 
 writeMessage(); //Calling a PHP Function
