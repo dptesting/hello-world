a4312d2441d8bd2ec94eb8d73bbba5638f1b20ec
SusieTech <susie.kellie@gmail.com>
2017-05-08 22:09:12 +0100

    Testing-Netbeans
    Deb's reminded me to test the code before putting it through Version Control and so this file contains a working code that was tested in Netbeans.

--- index.php
+++ index.php
@@ -6,13 +6,20 @@ function writeMessage() {//Defining a PHP Function
     echo "Hey Debs and Susie. Are you enjoying git???";
     echo "Susie and Jent - I dont like sourcetree";
 }
-
 writeMessage(); //Calling a PHP Function
-
 //Susie trying out version control with Debs
-function sayhello {
+function sayhello() {
     echo "Hello Debs!";
-    echo "Hello Janet!":
+    echo "Hello Janet";
     echo "Why tell us the coding test is harder than we expect!";
 }
+
+sayhello();
+
+//Susie Testing in Netbeans prior to sending version control back to Debs
+function testingtesting123() {
+    echo "Testing Testing 123";
+}
+
+testingtesting123();
 
